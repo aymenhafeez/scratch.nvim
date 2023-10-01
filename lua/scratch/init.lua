9bc9be8f931ea4d609f54da0b9338150fcf1015b
Aymen Hafeez <aymennh@gmail.com>
2023-10-01 01:47:41 +0100

    Added configurable default opts

--- lua/scratch/init.lua
+++ lua/scratch/init.lua
@@ -18,7 +18,7 @@ local scratch_win = nil
 
 function M.ToggleScratch(config)
   config = config or {}
-  local opts = config.opts or default_opts
+  local opts = vim.tbl_extend("force", default_opts, config)
 
   if scratch_buf and not vim.api.nvim_buf_is_valid(scratch_buf) then
     scratch_buf = nil
@@ -34,14 +34,11 @@ function M.ToggleScratch(config)
 
     local content = scratch_content[scratch_buf] or {}
     vim.api.nvim_buf_set_lines(scratch_buf, 0, -1, false, content)
-
-    vim.cmd([[startinsert]])
   else
     if vim.api.nvim_win_is_valid(scratch_win) then
       vim.api.nvim_win_hide(scratch_win)
     else
       scratch_win = vim.api.nvim_open_win(scratch_buf, true, opts)
-      vim.cmd([[startinsert]])
     end
   end
 end
@@ -53,8 +50,6 @@ function M.SaveScratch()
 end
 
 function M.setup(config)
-  default_opts = config.opts or default_opts
-
   vim.cmd([[command! Scratch lua require'scratch'.ToggleScratch(config)]])
 end
 
